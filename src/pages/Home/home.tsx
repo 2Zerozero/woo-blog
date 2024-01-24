f81ee9d51b321835c555d6b328311c9d9d8f5499
2Zerozero <hawgae@gmail.com>
2024-01-24 16:01:59 +0900

    [feat] 프로젝트 초기설정

---
+++ src/pages/Home/home.tsx
@@ -0,0 +1,21 @@
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="container">
+      {/* 왼쪽 네비게이션 */}
+      <div className="leftSection">
+        {/* 유저 */}
+        <div className="user">
+          <div>2Zerozero</div>
+          <button>버튼</button>
+        </div>
+      </div>
+
+      {/* 오른쪽 섹션 */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Home;
