002bc00bbf8251af7e8ab07745dc5c69100fd2f1
2Zerozero <hawgae@gmail.com>
2024-02-06 00:24:11 +0900

    [feat] CSS 수정

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,20 +1,34 @@
+import Header from '@/components/Header/header';
 import './home.scss';
 
 const Home = () => {
   return (
-    <div className="container">
-      {/* 왼쪽 네비게이션 */}
-      <div className="leftSection">
-        {/* 유저 */}
-        <div className="user">
-          <div>2Zerozero</div>
-          <button>버튼</button>
-        </div>
-      </div>
+    <>
+      {/* 헤더 */}
+      <Header />
 
-      {/* 오른쪽 섹션 */}
-      <div></div>
-    </div>
+      <div className="container">
+        {/* 왼쪽 네비게이션 */}
+        <div className="leftSection">
+          {/* 유저 */}
+          <div className="user">
+            <div>2Zerozero</div>
+            <button>버튼</button>
+          </div>
+          {/* 블로그 카테고리 */}
+          <div>
+            <ul>
+              <li>리스트 1</li>
+              <li>리스트 2</li>
+              <li>리스트 3</li>
+            </ul>
+          </div>
+        </div>
+
+        {/* 오른쪽 섹션 */}
+        <div className="rightSection"></div>
+      </div>
+    </>
   );
 };
 
